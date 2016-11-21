1786a7690fb6a76ffcd12bcbaaa4c871641ff9b9
Alex Windle <alexwindle85@gmail.com>
2016-11-21 16:05:00 -0500

    Add form submit button basic functionality.

--- src/js/controllers/add.js
+++ src/js/controllers/add.js
@@ -1,6 +1,14 @@
 function AddController($scope){
 	console.log('Activate AddController!');
 
+	// let vm = this;
+
+	// What is this doing? 
+	this.addUser = addUser;
+
+	function addUser(user){
+		console.log('Clicked Add button!');
+	};
 }
 
 AddController.$inject = ['$scope']; 
