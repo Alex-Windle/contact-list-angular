1a9e9530543c7f92f0ce24fce594bf9360b8135c
Alex Windle <alexwindle85@gmail.com>
2016-11-21 17:11:00 -0500

    Update AddController to POST new user.

--- src/js/controllers/add.js
+++ src/js/controllers/add.js
@@ -1,15 +1,19 @@
-function AddController($scope){
-	console.log('Activate AddController!');
+function AddController($state, UserService){
+	console.log('Activate AddController');
 
-	// let vm = this;
+	let vm = this;
 
-	// What is this doing? 
 	this.addUser = addUser;
 
 	function addUser(user){
 		console.log('Clicked Add button!');
+		UserService.addUser(user).then((resp) => {
+      	console.log('response', resp);
+      	$state.go('home');
+    });
+
 	};
 }
 
-AddController.$inject = ['$scope']; 
+AddController.$inject = ['$state', 'UserService']; 
 export { AddController }; 
